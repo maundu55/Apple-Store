4fef5f6441fca2773932afd03b0e3933b11e2a28
maundu55 <marymaundu.mm@gmail.com>
2025-03-19 15:19:36 +0300

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   const[frameZoom, setFrameZoom]= useState(false)
+  const[activePage, setActivePage]= useState(0)
 
+  const handleNavClick=(pageIndex)=>{
+    setActivePage(pageIndex)
+  }
   const toggleZoom =()=>{
     setFrameZoom(!frameZoom)
   }
@@ -13,7 +17,7 @@ const App = () => {
   return (
     <div className='w-full h-screen grid place-items-center'>
       <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
-        <Navbar />
+        <Navbar activePage={activePage} handleNavClick={handleNavClick} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
       </div>
     </div>
