4e0438487ba369c8d9f65acbcb59105e4e2e7b30
maundu55 <marymaundu.mm@gmail.com>
2025-03-19 15:31:36 +0300

    add Home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import Controls from './components/Controls'
 import Navbar from './components/Navbar'
-
+import Home from './components/Home'
 
 const App = () => {
   const[frameZoom, setFrameZoom]= useState(false)
@@ -16,9 +16,12 @@ const App = () => {
 
   return (
     <div className='w-full h-screen grid place-items-center'>
-      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100`}>
+      <div className={`${frameZoom && 'min-w-[97vw] min-h-[97vh]'} w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh] border border-gray-300 rounded-2xl resize overflow-auto relative transition-all duration-100 flex`}>
         <Navbar activePage={activePage} handleNavClick={handleNavClick} />
         <Controls toggleZoom={toggleZoom} frameZoom={frameZoom} />
+        <div className='flex-grow'>
+        <Home />
+        </div>
       </div>
     </div>
   )
