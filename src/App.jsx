3978c04cae9804e581240f8f13113bb7e42a0ccf
maundu55 <marymaundu.mm@gmail.com>
2025-03-19 12:58:33 +0300

    first commit -Apple Product Store ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen grid place-items-center'>
+      <div className='w-[70vw] h-[85vh] min-w-[70vw] min-h-[85vh] max-w-[90vw] max-h-[90vh]'>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
