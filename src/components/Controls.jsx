f6da95f6c4fe7310d192cbe3a27d8ab67149bf2d
maundu55 <marymaundu.mm@gmail.com>
2025-03-19 13:44:41 +0300

    add zoom functionality

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Controls = () => {
+const Controls = ({ toggleZoom, frameZoom }) => {
   return (
     <div className='absolute top-3 right-3 space-x-2'>
-        <button className='text-2xl text-pink-400 cursor-pointer'>
-            <i className='bx bxs-zoom-in'></i>
+        <button className='text-2xl text-pink-400 cursor-pointer' onClick={toggleZoom}>
+            <i className={frameZoom ? 'bx bxs-zoom-out' : 'bx bxs-zoom-in'}></i>
         </button>
         <button className='text-2xl text-pink-400 '>
             <i className='bx bx-x-circle'></i>
